2726b79247f33bea967cdf192656031ada097a38
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-12 23:56:27 +0100

    Initial commit

---
+++ EscapeChar.py
@@ -0,0 +1,10 @@
+SplitString = "This string \nhas \nbeen \nsplit"
+
+print(SplitString)
+
+TabString = "This string \thas \tbeen \tTabbed"
+
+print(TabString)
+
+print('The Pet Shop \'Owner \'said \'no')
+
---
+++ Strings.py
@@ -0,0 +1,19 @@
+print("Today is a good day to learn Python")
+print('Test')
+print('We Can "s" WOW')
+print("Hello " + "World")
+
+Greeting = "Hello"
+Name = "Josh"
+
+print(Greeting + " " + Name)
+
+age = 24
+print(age)
+
+print(type(Greeting))
+print(type(age))
+
+age = "Wow"
+print(age)
+print(type(age))
