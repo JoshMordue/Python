9e32c61c84b8f641b9eaf5bf71833f9f07433f4c
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-14 16:29:09 +0100

    Further work on the Course

--- Basic/Strings.py
+++ Basic/Strings.py
@@ -14,6 +14,15 @@ print(age)
 print(type(Greeting))
 print(type(age))
 
-age = "Wows"
+age = 27
 print(age)
 print(type(age))
+
+age_in_words = "2 years"
+print(Name + f" is {age} Years old")
+print(type(age))
+
+
+print(f"Pi is approximately {22/7:12.50f}")
+pi = 22 / 7
+print(f"Pi is approximately {pi:12.50f}")
