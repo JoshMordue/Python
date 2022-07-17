ada15ee29e24cc6083e832955d1927112e49035a
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-17 11:53:12 +0100

    Update GuessingGame.py

--- ProgramFlowControl(PartTwo)/GuessingGame.py
+++ ProgramFlowControl(PartTwo)/GuessingGame.py
@@ -1,20 +1,46 @@
-answer = 5
+import random
 
-print("Please guess a number between 1 and 10: ")
-guess = int(input())
+highest = 10
+answer = random.randint(1, highest)
+guesses = 1
+guess = 0
 
-if guess == answer:
-    print("You got it first try!")
-else:
-    if guess < answer:
-        print("Please guess higher!")
-    if guess > answer:
-        print("Please guess Lower!")
+print(answer) #for testing purposes
+
+
+while guess != answer:
+    print("Please guess a number between 1 to {0}: ".format(highest))
     guess = int(input())
-    if guess == answer:
-        print("Well done, you've guessed it correctly!")
+
+    if guess == 0:
+        print("You gave up.")
+        break
+    elif guess == answer:
+        print("You got it!")
+        print("Amount of guesses it took: {0}".format(guesses))
+        break
     else:
-        print("Sorry you guessed it incorrectly again")
+        if guess < answer:
+             print("Please guess higher!")
+        if guess > answer:
+             print("Please guess Lower!")
+        guesses += guesses
+
+
+
+#
+# elif guess == 0:
+#     print("You gave up.")
+# else:
+#     if guess < answer:
+#         print("Please guess higher!")
+#     if guess > answer:
+#         print("Please guess Lower!")
+#     guess = int(input())
+#     if guess == answer:
+#         print("Well done, you've guessed it correctly!")
+#     else:
+#         print("Sorry you guessed it incorrectly again")
 
 # if guess < answer:
 #     print("Please guess higher!")
