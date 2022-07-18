d81e376aa8a78b6bb3ef6f299672d2cb958be3af
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-18 17:35:23 +0100

    Create NumberLists.py

---
+++ Part3(ListsandTuples)/NumberLists.py
@@ -0,0 +1,18 @@
+even = [2, 4, 6, 8]
+
+odd = [1, 3, 5, 7, 9]
+
+print(max(even))
+print(min(even))
+
+print(max(odd))
+print(min(odd))
+
+print()
+
+print(len(even))
+print(len(odd))
+
+print()
+
+print("Mississippi".count("s"))
