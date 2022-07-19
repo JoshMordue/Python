3da2cb0826113f6b754c5158755c761b2d0c9bba
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-19 19:17:39 +0100

    Update Outliers.py

--- Part3(ListsandTuples)/Outliers.py
+++ Part3(ListsandTuples)/Outliers.py
@@ -16,10 +16,27 @@ data = [3, 4, 6, 100, 102, 150, 152, 1466]
 min_value = 100
 max_value = 150
 
+# process the low values in the list
+
 for index, value in enumerate(data):
-    if (value < min_value) or (value > max_value):
-        del data[index]
-        
+    if value >= min_value:
+        stop = index
+        break
 
+print(data)
+del data[:stop]
+print(data)
 
+# process high values in the list
+start = 0
+
+for index in range(len(data) - 1, -1, -1):
+    if data[index] <= max_value:
+        # we have the index of last item to keep
+        # set start to the position of the first item to delete
+        # which is 1 after 'index'
+        start = index + 1
+        break
+
+del data[start:]
 print(data)
