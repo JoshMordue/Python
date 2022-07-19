c669b8fee18cb0c4ce8a88515c7cd9d41c626d37
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-19 19:06:57 +0100

    Create Outliers.py

---
+++ Part3(ListsandTuples)/Outliers.py
@@ -0,0 +1,9 @@
+computer_parts = ["computer",
+                  "monitor",
+                  "keyboard",
+                  "mouse",
+                  "mouse mat"
+                  ]
+
+del computer_parts[0:2]
+print(computer_parts)
