464353556808c88113ff75501b467e41f8b130a6
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-19 19:11:11 +0100

    Update Outliers.py

--- Part3(ListsandTuples)/Outliers.py
+++ Part3(ListsandTuples)/Outliers.py
@@ -1,9 +1,25 @@
-computer_parts = ["computer",
-                  "monitor",
-                  "keyboard",
-                  "mouse",
-                  "mouse mat"
-                  ]
+# computer_parts = ["computer",
+#                   "monitor",
+#                   "keyboard",
+#                   "mouse",
+#                   "mouse mat"
+#                   ]
 
-del computer_parts[0:2]
-print(computer_parts)
+# del computer_parts[0:2]
+# print(computer_parts)
+#
+# del computer_parts[-2:]
+# print(computer_parts)
+
+data = [3, 4, 6, 100, 102, 150, 152, 1466]
+
+min_value = 100
+max_value = 150
+
+for index, value in enumerate(data):
+    if (value < min_value) or (value > max_value):
+        del data[index]
+        
+
+
+print(data)
