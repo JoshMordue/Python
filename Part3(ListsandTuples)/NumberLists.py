3c55840b879ab00a7f329d5f5701ca4ba8074e53
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-20 16:33:35 +0100

    work on project

--- Part3(ListsandTuples)/NumberLists.py
+++ Part3(ListsandTuples)/NumberLists.py
@@ -2,27 +2,10 @@ even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 empty_list = []
 
-numbers = even + odd
+numbers = [even, odd]
 print(numbers)
 
-sorted_numbers = sorted(numbers)
-print(sorted_numbers)
-
-digits = sorted("432985617")
-print(digits)
-
-more_numbers = list(numbers)
-print(more_numbers)
-
-
-# even.extend(odd)
-# print(even)
-#
-# another_even = even
-# print(another_even)
-#
-# even.sort(reverse=True)
-#
-# print(even)
-# print(another_even)
-
+for number_list in numbers:
+    print(number_list)
+    for value in number_list:
+        print(value)
