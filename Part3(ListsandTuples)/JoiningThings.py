550c4ea22e67d8931968cb783f9ef18f8409d8b5
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-20 22:20:50 +0100

    Create JoiningThings.py

---
+++ Part3(ListsandTuples)/JoiningThings.py
@@ -0,0 +1,19 @@
+flowers = [
+    "Dafffodil",
+    "Evening Primrose",
+    "Hydrangea",
+    "Iris",
+    "Sunflower",
+    "Tiger lily",
+    "Lavender"
+]
+
+# for flower in flowers:
+#     print(flower)
+
+separator = ", "
+output = separator.join(flowers)
+
+print(output)
+
+print(", ".join(flowers))
