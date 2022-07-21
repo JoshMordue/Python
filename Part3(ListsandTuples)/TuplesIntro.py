03923010bf2109e006add41425d66063092f6295
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-21 19:11:00 +0100

    Update TuplesIntro.py

--- Part3(ListsandTuples)/TuplesIntro.py
+++ Part3(ListsandTuples)/TuplesIntro.py
@@ -1,27 +1,12 @@
-welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
-bad = "Bad Company", "Bad Company", 1974
-budgie = "Nightflight", "Budgie", 1981
-imelda = "More Mayhem", "Emilda May", 2011
-metallica = "Ride the Lightning", "Metallica", 1984
+albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
+          ("Bad Company", "Bad Company", 1974),
+          ("Nightflight", "Budgie", 1981),
+          ("More Mayhem", "Emilda May", 2011),
+          ("Ride the Lightning", "Metallica", 1984),
+          ]
 
-title, artist, year = metallica
-print(title)
-print(artist)
-print(year)
+print(len(albums))
 
-table = ("Coffee Table", 200, 150, 100, 34.50)
-
-name, length, width, height, price = table
-
-print(length * width)
-
-
-# print(metallica[0])
-# print(metallica[1])
-# print(metallica[2])
-#
-# metallica2 = list(metallica)
-# print(metallica2)
-#
-# metallica2[2] = "Master of Puppets"
-# print(metallica2)
+for name, artist, year in albums:
+    print("Album: {}, Artist: {}, Year: {}"
+          .format(name, artist, year))
