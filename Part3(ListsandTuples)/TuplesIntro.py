dd22bd31c156a1d54d4e6c9b510e6d0eb2ec3842
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-21 18:13:42 +0100

    Update TuplesIntro.py

--- Part3(ListsandTuples)/TuplesIntro.py
+++ Part3(ListsandTuples)/TuplesIntro.py
@@ -1,3 +1,17 @@
-t = ("a", "b", "c")
+welcome = "Welcome to my Nightmare", "Alice Cooper", 1975
+bad = "Bad Company", "Bad Company", 1974
+budgie = "Nightflight", "Budgie", 1981
+imelda = "More Mayhem", "Emilda May", 2011
+metallica = "Ride the Lightning", "Metallica", 1984
 
-print(t)
+print(metallica)
+
+print(metallica[0])
+print(metallica[1])
+print(metallica[2])
+
+metallica2 = list(metallica)
+print(metallica2)
+
+metallica2[2] = "Master of Puppets"
+print(metallica2)
