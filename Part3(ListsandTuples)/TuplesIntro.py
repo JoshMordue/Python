04701340f3782aa66c2d1adc7769287c7ec0a027
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-21 18:56:19 +0100

    Update TuplesIntro.py

--- Part3(ListsandTuples)/TuplesIntro.py
+++ Part3(ListsandTuples)/TuplesIntro.py
@@ -4,14 +4,24 @@ budgie = "Nightflight", "Budgie", 1981
 imelda = "More Mayhem", "Emilda May", 2011
 metallica = "Ride the Lightning", "Metallica", 1984
 
-print(metallica)
+title, artist, year = metallica
+print(title)
+print(artist)
+print(year)
 
-print(metallica[0])
-print(metallica[1])
-print(metallica[2])
+table = ("Coffee Table", 200, 150, 100, 34.50)
 
-metallica2 = list(metallica)
-print(metallica2)
+name, length, width, height, price = table
 
-metallica2[2] = "Master of Puppets"
-print(metallica2)
+print(length * width)
+
+
+# print(metallica[0])
+# print(metallica[1])
+# print(metallica[2])
+#
+# metallica2 = list(metallica)
+# print(metallica2)
+#
+# metallica2[2] = "Master of Puppets"
+# print(metallica2)
