c88132feae7957152cdd8629629ac2745aa969bc
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-22 13:19:29 +0100

    Update Palindrome.py

--- Part4(Functions)/Palindrome.py
+++ Part4(Functions)/Palindrome.py
@@ -3,13 +3,30 @@ def is_palindrome(string):
     # return backwards == string
     return string[::-1].casefold() == string.casefold()
 
+def is_palindrome_sentence(sentence):
+    string = ""
+    for char in sentence:
+        if char.isalnum():
+            string += char
+
+    return string[::-1].casefold() == string.casefold()
+
+
 word = input("Please enter a word to check: ")
 
-if is_palindrome(word):
+
+
+
+
+if is_palindrome_sentence(word):
     print("'{}' is a palindrome".format(word))
 else:
     print("'{}' is not a palindrome".format(word))
 
+# if is_palindrome(word):
+#     print("'{}' is a palindrome".format(word))
+# else:
+#     print("'{}' is not a palindrome".format(word))
 
 
 
