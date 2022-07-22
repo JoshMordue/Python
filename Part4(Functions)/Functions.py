8d5e77c4e655a4fc5c9104aa0ff9c1c84c1c9fda
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-22 12:58:26 +0100

    Update Functions.py

--- Part4(Functions)/Functions.py
+++ Part4(Functions)/Functions.py
@@ -4,5 +4,13 @@ def multiply(x, y):
 
 
 answer = multiply(40, 10)
-
 print(answer)
+
+forty_two = multiply(6, 7)
+print(forty_two)
+
+print()
+
+for val in range (1, 5):
+    two_times = multiply(2, val)
+    print(two_times)
