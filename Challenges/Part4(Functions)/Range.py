fe35880783bb7afe4484996a06a54408d61f5659
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-24 01:51:17 +0100

    Update Range.py

--- Challenges/Part4(Functions)/Range.py
+++ Challenges/Part4(Functions)/Range.py
@@ -1,4 +1,12 @@
 def sum_eo(n, t):
+    """
+    The method takes two of the users inputs, a positive number
+    and if it's odd or even to calculate the sum of all the numbers
+    that are odd or even from 0 to equal the number provided.
+    :param n: the positive number
+    :param t: the users input for odd or even
+    :return: the sum of the range, if an invalid character has been entered it returns 0
+    """
     if n >= 0:
         if t == 'e':
             return sum(range(2, n, 2))
