d74e1361024bb5f70cb51ad37ec8f9d15e03f42a
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-25 17:55:22 +0100

    Create Banner.py

---
+++ Part4(Functions)/Banner.py
@@ -0,0 +1,12 @@
+def banner_text(text):
+    screen_width = 80
+    if len(text) > screen_width - 4:
+        print("EEK!!")
+        print("The text is too long to fit in the specified width")
+
+    if text == "*":
+        print("*" * screen_width)
+    else:
+        output_string = "**{0}**".format(text.center(screen_width - 4))
+        print(output_string)
+    
