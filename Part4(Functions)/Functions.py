2bc6ccfe4ad32f2e80fff4d56f337408ccba10d9
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-26 14:50:49 +0100

    Update Functions.py

--- Part4(Functions)/Functions.py
+++ Part4(Functions)/Functions.py
@@ -1,4 +1,16 @@
 def multiply(x, y):
+    """
+    Multiply 2 numbers.
+
+    Although this function is intended to multiply 2 numbers,
+    it can also be used to multiply a sequence.
+    If you pass a String through the 'x' variable it will get repeated
+    by the value 'y' as the returned value.
+
+    :param x: The first number to multiply
+    :param y: the number to multiply 'x' by.
+    :return: the product of multiplying 'x' and 'y'
+    """
     result = x * y
     return result
 
