9e980fc7dead8d1e8196058d6b2a11f9ae938351
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-26 14:27:34 +0100

    Update Banner.py

--- Part4(Functions)/Banner.py
+++ Part4(Functions)/Banner.py
@@ -1,5 +1,4 @@
-def banner_text(screen_width, text):
-
+def banner_text(text, screen_width=80):
     if len(text) > screen_width - 4:
         raise ValueError("String {0} is larger than specified width {1}"
                          .format(text, screen_width))
@@ -11,16 +10,16 @@ def banner_text(screen_width, text):
         print(output_string)
 
 
-banner_text(80, "*")
-banner_text(80, "Always look on the bright side of life...")
-banner_text(80, "If life seems jolly rotten,")
-banner_text(80, "There's something you've forgotten!")
-banner_text(80, "And that's to laugh and smile and dance and sing,")
-banner_text(80, " ")
-banner_text(80, "When you're feeling in the dumps,")
-banner_text(80, "Don't be silly chumps,")
-banner_text(80, "Just purse your lips and whistle - that's the thing!")
-banner_text(80, "And... always look on the bright side of life...")
-banner_text(80, "*")
+banner_text("*")
+banner_text("Always look on the bright side of life...")
+banner_text("If life seems jolly rotten,")
+banner_text("There's something you've forgotten!")
+banner_text("And that's to laugh and smile and dance and sing,")
+banner_text(" ")
+banner_text("When you're feeling in the dumps,")
+banner_text("Don't be silly chumps,")
+banner_text("Just purse your lips and whistle - that's the thing!")
+banner_text("And... always look on the bright side of life...")
+banner_text("*")
 
 
