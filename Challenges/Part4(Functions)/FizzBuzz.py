885d37fb3bfa4ef278f50d6c7048547ee12adee4
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-27 17:29:16 +0100

    Update FizzBuzz.py

--- Challenges/Part4(Functions)/FizzBuzz.py
+++ Challenges/Part4(Functions)/FizzBuzz.py
@@ -1,18 +1,35 @@
 def fizz_buzz(number: int) -> str:
     """
-    Plays FizzBuzz
-    :param number: the number to check
-    :return: "Fizz" if it's a number divisible by 3, "Buzz" if it's divisible by 5
-        "Fizz Buzz" if it's divisible by 15.
+    Play Fizz buzz
+
+    :param number: The number to check.
+    :return: 'fizz' if the number is divisible by 3.
+        'buzz' if it's divisible by 5.
+        'fizz buzz' if it's divisible by both 3 and 5.
+        The number, as a string, otherwise.
     """
     if number % 15 == 0:
         return "fizz buzz"
-    if number % 5 == 0:
-        return "buzz"
-    if number % 3 == 0:
+    elif number % 3 == 0:
         return "fizz"
-    return str(number)
+    elif number % 5 == 0:
+        return "buzz"
+    else:
+        return str(number)
 
 
-for i in range(1, 101):
-    print(fizz_buzz(i))
+input("Play Fizz Buzz.   Press ENTER to start")
+print()
+
+next_number = 0
+while next_number < 99:
+    next_number += 1
+    print(fizz_buzz(next_number))
+    next_number += 1
+    correct_answer = fizz_buzz(next_number)
+    players_answer = input("Your go: ")
+    if players_answer != correct_answer:
+        print("You lose, the correct answer was {}".format(correct_answer))
+        break
+else:
+    print("Well done, you reached {}".format(next_number))
