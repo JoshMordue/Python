24647dc5f53c802f57d5fdaa6c8a36f34a3fe3a6
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-28 13:26:40 +0100

    Update Factorial.py

--- Challenges/Part4(Functions)/Factorial.py
+++ Challenges/Part4(Functions)/Factorial.py
@@ -1,14 +1,29 @@
+# def factorial(counter: int) -> int:
+#     """
+#     function to find the factorial number of the 'Int' passed through
+#
+#     :param counter: Int
+#     :return: the factorial number as an integer
+#     """
+#     if counter == 0:
+#         return 1
+#     else:
+#         return counter*factorial(counter-1)
+#
+#
+# for i in range(0, 36):
+#     print(i, factorial(i))
+
 def factorial(counter: int) -> int:
     """
-    function to find the factorial number of the 'Int' passed through
-
-    :param counter: Int
-    :return: the factorial number as an integer
+    function to find the factorial number of the int passed through
     """
-    if counter == 0:
-        return 1
-    else:
-        return counter*factorial(counter-1)
+    result = 1
+
+    for j in range(result, counter):
+        result *= j
+        j += 1
+    return result
 
 
 for i in range(0, 36):
