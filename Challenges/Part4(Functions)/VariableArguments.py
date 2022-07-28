38e942f809a7c30ff5d33ff503fc5149dec0d847
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-07-28 18:38:58 +0100

    Update VariableArguments.py

--- Challenges/Part4(Functions)/VariableArguments.py
+++ Challenges/Part4(Functions)/VariableArguments.py
@@ -6,10 +6,12 @@ def sum_numbers(*numbers: float) -> float:
     return result
 
 
-#def sum_numbers(*numbers: float) -> float:
-#   """Returns the value of all the variables passed through as a 'Float'"""
-    #return sum(numbers)
-    #a simpler way of solving it using inbuilt functions
-
 print(sum_numbers(1, 2, 3))
 print(sum_numbers(12.5, 6.25, 11.778))
+
+
+# def sum_numbers(*numbers: float) -> float:
+#   """Returns the value of all the variables passed through as a 'Float'"""
+#    #return sum(numbers)
+#    a simpler way of solving it using inbuilt functions
+
