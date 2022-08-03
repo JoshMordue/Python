09dc88f98952f977691e1bd6df4b19645becefc0
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-03 13:04:42 +0100

    Update MealPlanner.py

--- Part5(Dictionaries&Sets)/MealPlanner.py
+++ Part5(Dictionaries&Sets)/MealPlanner.py
@@ -1,2 +1,19 @@
 from Contents import pantry, recipes
 
+# display_dict = {str(index + 1): meal for index, meal in enumerate(recipes)}
+display_dict = {}
+
+for index, key in enumerate(recipes):
+    display_dict[str(index + 1)] = key
+
+while True:
+    # display a menu of the recipes we know how to cook
+    print("Please choose your recipe")
+    print("-" * 10)
+    for key, value in display_dict.items():
+        print(f"{key} - {value}")
+
+    choice = input(": ")
+
+    if choice == "0":
+        break
