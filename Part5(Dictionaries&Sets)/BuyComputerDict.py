735fcc9419d30533976df45da0df52f5c0b484d3
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-03 12:28:22 +0100

    Update BuyComputerDict.py

--- Part5(Dictionaries&Sets)/BuyComputerDict.py
+++ Part5(Dictionaries&Sets)/BuyComputerDict.py
@@ -1,36 +1,19 @@
-available_parts = ["computer",
-                   "monitor",
-                   "keyboard",
-                   "mouse",
-                   "HDMI",
-                   "DVD Drive"
-                   ]
+available_parts = {"1": "computer",
+                   "2": "monitor",
+                   "3": "keyboard",
+                   "4": "mouse",
+                   "5": "HDMI",
+                   "6": "DVD Drive"
+                   }
 
-#valid_choices = [str(i) for i in range(1, len(available_parts) + 1)]
-valid_choices = []
-for i in range(1, len(available_parts) + 1):
-    valid_choices.append(str(i))
-current_choice = "-"
-computer_parts = []
+current_choice = None
 
-while current_choice != '0':
-    if current_choice in valid_choices:
-        index = int(current_choice) - 1
-        chosen_part = available_parts[index]
-        if chosen_part in computer_parts:
-            print("Removing {}".format(current_choice))
-            #It's already in so remove it
-            computer_parts.remove(chosen_part)
-        else:
-            print("Adding {}".format(current_choice))
-            computer_parts.append(chosen_part)
-            print("Your list now contains: {}".format(computer_parts))
-
+while current_choice != "0":
+    if current_choice in available_parts:
+        chosen_part = available_parts[current_choice]
+        print(f"Adding {chosen_part}")
     else:
-        print("Please add options from the list below:")
-        for id, part in enumerate(available_parts):
-            print("{0}: {1}".format(id + 1, part))
+        print("Invalid entry please see below the valid options: ")
+    print(available_parts)
 
-    current_choice = input()
-
-print(computer_parts)
+    current_choice = input("> ")
