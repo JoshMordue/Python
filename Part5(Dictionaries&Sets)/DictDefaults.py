a1224653401b2c68c39129d116ccc22588e0ada5
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-04 23:57:00 +0100

    Update DictDefaults.py

--- Part5(Dictionaries&Sets)/DictDefaults.py
+++ Part5(Dictionaries&Sets)/DictDefaults.py
@@ -1,5 +1,10 @@
 from Contents import pantry
 
 chicken_quantity = pantry.setdefault("chicken", 0)
-
 print(f"chicken: {chicken_quantity}")
+
+beans_quantity = pantry.setdefault("beans", 0)
+print(f"beans: {beans_quantity}")
+
+ketchup_quantity = pantry.setdefault("ketchup", 0)
+print(f"ketchup: {ketchup_quantity}")
