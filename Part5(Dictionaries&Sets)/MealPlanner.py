4ed6dbaf3711595f037abdb31006244f1e286064
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-04 19:18:09 +0100

    Update MealPlanner.py

--- Part5(Dictionaries&Sets)/MealPlanner.py
+++ Part5(Dictionaries&Sets)/MealPlanner.py
@@ -39,5 +39,5 @@ while True:
                 print(f"\tYou need to buy {quantity_to_buy} of {food_item}")
                 add_shopping_item(shopping_list, food_item, quantity_to_buy)
 
-for things in shopping_list:
-    print(things)
+for items in shopping_list:
+    print(items)
