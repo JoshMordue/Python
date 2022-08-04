739b12b5ddd2bd3204279f19589e45a275ccd906
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-04 18:55:52 +0100

    work on the course

--- Part5(Dictionaries&Sets)/MealPlanner.py
+++ Part5(Dictionaries&Sets)/MealPlanner.py
@@ -25,8 +25,10 @@ while True:
         ingredients = recipes[selected_item]
         print(ingredients)
 
-        for food_item in ingredients:
-            if food_item in pantry:
+        for food_item, required_quantity in ingredients.items():
+            quantity_in_pantry = pantry.get(food_item, 0)
+            if required_quantity <= quantity_in_pantry:
                 print(f"\t {food_item} OK")
             else:
-                print(f"\t You don't have a necessary ingredient: {food_item}")
+                quantity_to_buy = required_quantity - quantity_in_pantry
+                print(f"\t You need to buy: {quantity_to_buy} of {food_item}")
