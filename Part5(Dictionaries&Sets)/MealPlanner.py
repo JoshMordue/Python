073946c33b6022cd6982defe8d989e1a3b477cbb
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-04 19:26:26 +0100

    Update MealPlanner.py

--- Part5(Dictionaries&Sets)/MealPlanner.py
+++ Part5(Dictionaries&Sets)/MealPlanner.py
@@ -1,12 +1,12 @@
 from Contents import pantry, recipes
 
 
-def add_shopping_item(data: list, item: str, amount: int) -> None:
-    """Add a tuple containing `item` and `amount` to the `data` list."""
-    for item in data:
-        if item in data:
-            data[item] += amount
-        data.append((item, amount))
+def add_shopping_item(data: dict, item: str, amount: int) -> None:
+    """Add a tuple containing `item` and `amount` to the `data` dict."""
+    if item in data:
+        data[item] += amount
+    else:
+        data[item] = amount
 
 
 # display_dict = {str(index + 1): meal for index, meal in enumerate(recipes)}
@@ -14,7 +14,7 @@ display_dict = {}
 for index, key in enumerate(recipes):
     display_dict[str(index + 1)] = key
 
-shopping_list = []
+shopping_list = {}
 
 while True:
     # Display a menu of the recipes we know how to cook
@@ -42,5 +42,5 @@ while True:
                 print(f"\tYou need to buy {quantity_to_buy} of {food_item}")
                 add_shopping_item(shopping_list, food_item, quantity_to_buy)
 
-for items in sorted(shopping_list):
-    print(items)
+for things in shopping_list.items():
+    print(things)
