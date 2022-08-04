9a86ccabb6b9a0f11eb5983187fbd01e24d3d8c2
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-05 00:00:16 +0100

    Update DictDefaults.py

--- Part5(Dictionaries&Sets)/DictDefaults.py
+++ Part5(Dictionaries&Sets)/DictDefaults.py
@@ -9,6 +9,8 @@ print(f"beans: {beans_quantity}")
 ketchup_quantity = pantry.get("ketchup", 0)
 print(f"ketchup: {ketchup_quantity}")
 
+z_quantity = pantry.setdefault("zucchini", "eight")
+print(f"zucchini: {z_quantity}")
 print()
 print("'pantry' now contains...")
 
