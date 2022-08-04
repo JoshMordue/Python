2e96fd5b4f967115ddbd181825a6a77dcb58d5ee
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-04 16:33:34 +0100

    Update MealPlanner.py

--- Part5(Dictionaries&Sets)/MealPlanner.py
+++ Part5(Dictionaries&Sets)/MealPlanner.py
@@ -24,3 +24,9 @@ while True:
         print("Checking ingredients ...")
         ingredients = recipes[selected_item]
         print(ingredients)
+
+        for food_item in ingredients:
+            if food_item in pantry:
+                print(f"\t {food_item} OK")
+            else:
+                print(f"\t You don't have a necessary ingredient: {food_item}")
