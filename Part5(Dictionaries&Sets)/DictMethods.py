c1ebb388b0511f5b0c0d5c57230366534fd6ebf3
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-06 15:30:29 +0100

    Create DictMethods.py

---
+++ Part5(Dictionaries&Sets)/DictMethods.py
@@ -0,0 +1,16 @@
+d = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+}
+
+pantry_items = ['chicken', 'spam', 'egg', 'bread', 'lemon']
+
+new_dict = 
