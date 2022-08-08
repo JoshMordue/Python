4dbfa02e1db0d2d73677b95d93be7a384895886b
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-09 00:04:49 +0100

    Update Copy.py

--- Part5(Dictionaries&Sets)/Copy.py
+++ Part5(Dictionaries&Sets)/Copy.py
@@ -1,19 +1,22 @@
+import copy
+
 animals = {
     "lion": ["scary", "big", "cat"],
-    "elephant": ["big", "grey"],
-    "teddy": ["cuddly", "stuffed"]
+    "elephant": ["big", "grey", "wrinkled"],
+    "teddy": ["cuddly", "stuffed"],
 }
 
+# Perform a shallow copy
 things = animals.copy()
 
-animals["teddy"] = "toy"
+# Perform a deep copy
+# things = copy.deepcopy(animals)
 
-print(things["teddy"])
-print(animals["teddy"])
+print(id(things["teddy"]), things["teddy"])
+print(id(animals["teddy"]), animals["teddy"])
 
 print()
 
 things["teddy"].append("toy")
-
 print(things["teddy"])
 print(animals["teddy"])
