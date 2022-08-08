a7e8aa903ea8ba5c25ddfbb50ca111b999a188d2
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-08 23:18:20 +0100

    Update DictMethods.py

--- Part5(Dictionaries&Sets)/DictMethods.py
+++ Part5(Dictionaries&Sets)/DictMethods.py
@@ -9,26 +9,35 @@ d = {
     7: "seven",
     8: "eight",
     9: "nine",
-    11: "eleven",
 }
 
 pantry_items = ['chicken', 'spam', 'egg', 'bread', 'lemon']
 
-d2 = {
-    7:  "lucky seven",
-    10: "ten",
-    3:  "this is the new three",
-}
+# Code for the dict update method lecture
+v = d.values()
+print(v)
 
-d.update(d2)
-for key, value in d.items():
-    print(key, value)
+d[10] = "ten"
+print(v)
 
-print()
+print("four" in v)
+print("eleven" in v)
 
-d.update(enumerate(pantry_items))
-for key, value in d.items():
-    print(key, value)
+# d2 = {
+#     7:  "lucky seven",
+#     10: "ten",
+#     3:  "this is the new three",
+# }
+#
+# d.update(d2)
+# for key, value in d.items():
+#     print(key, value)
+#
+# print()
+#
+# d.update(enumerate(pantry_items))
+# for key, value in d.items():
+#     print(key, value)
 
 # Code for "The remaining `dict` methods" lecture.
 # ************************************************
