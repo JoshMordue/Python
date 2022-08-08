72056843a37e60101189bb7eddbcd9e8397765dd
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-08 23:35:52 +0100

    Update Copy.py

--- Part5(Dictionaries&Sets)/Copy.py
+++ Part5(Dictionaries&Sets)/Copy.py
@@ -1,10 +1,19 @@
 animals = {
-    "lion": "scary",
-    "elephant": "big",
-    "teddy": "cuddly",
+    "lion": ["scary", "big", "cat"],
+    "elephant": ["big", "grey"],
+    "teddy": ["cuddly", "stuffed"]
 }
 
 things = animals.copy()
 
 animals["teddy"] = "toy"
+
 print(things["teddy"])
+print(animals["teddy"])
+
+print()
+
+things["teddy"].append("toy")
+
+print(things["teddy"])
+print(animals["teddy"])
