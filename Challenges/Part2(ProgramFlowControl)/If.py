79e2822454dca441953972edeafb6ec8ed8c0da6
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-12 22:03:29 +0100

    tidying work

--- Challenges/Part2(ProgramFlowControl)/If.py
+++ Challenges/Part2(ProgramFlowControl)/If.py
@@ -1,5 +1,6 @@
-#Write a small program to ask for a name and an Age.
-# When both values have been entered, check if the person is the right age to go on an 18-30 holiday. They must be over 18 and under 31.
+# Write a small program to ask for a name and an Age.
+# When both values have been entered, check if the person is the right age to go on an 18-30 holiday.
+# They must be over 18 and under 31.
 # If they are welcome them to the holiday otherwise print a polite message refusing them entry.
 
 print("Welcome to the admissions process")
