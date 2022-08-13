03d180c6f4004155c11bc9f4066cf58bc1c49304
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-13 20:34:37 +0100

    Update SetIntro.py

--- Part5(Dictionaries&Sets)/SetIntro.py
+++ Part5(Dictionaries&Sets)/SetIntro.py
@@ -3,3 +3,12 @@ print(farm_animals)
 
 for animal in farm_animals:
     print(animal)
+
+print()
+print("Indexing a sequence")
+animals_list = ['cow', 'sheep', 'hen', 'goat', 'horse']
+goat = animals_list[3]
+print(goat)
+
+print("Indexing a set is not possible")
+goat = farm_animals[3]
