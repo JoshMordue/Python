d924f12b13f80f451474daf51779e8c93377f944
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-13 23:09:43 +0100

    Update RemoveItemsSet.py

--- Part5(Dictionaries&Sets)/RemoveItemsSet.py
+++ Part5(Dictionaries&Sets)/RemoveItemsSet.py
@@ -1,5 +1,13 @@
 small_ints = set(range(21))
 print(small_ints)
 
-small_ints.clear()
+# clears the set
+# small_ints.clear()
+# print(small_ints)
+
+# removing entry 10 and 11
+small_ints.discard(10)
+small_ints.remove(11)
 print(small_ints)
+
+# discard crashes if the entry has not been found, whereas discard does not
