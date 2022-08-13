f6348cb75738ebb106e297e44a9daad509a465db
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-13 21:55:04 +0100

    Update ModifyingSets.py

--- Part5(Dictionaries&Sets)/ModifyingSets.py
+++ Part5(Dictionaries&Sets)/ModifyingSets.py
@@ -1,3 +1,11 @@
-numbers = {}
+numbers = set()
 
 print(numbers, type(numbers))
+
+# numbers.add(1)
+# print(numbers)
+
+while len(numbers) < 4:
+    next_value = int(input("Please enter your next value: "))
+    numbers.add(next_value)
+print(numbers)
