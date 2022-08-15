f02198ed07ea2c43eb2752e850f831448c63c0a4
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-15 16:57:24 +0100

    Update PrescriptionTrial.py

--- Part5(Dictionaries&Sets)/PrescriptionTrial.py
+++ Part5(Dictionaries&Sets)/PrescriptionTrial.py
@@ -1,10 +1,14 @@
 from PrescriptionData import *
 
-trial_patients = ["Denise", "Eddie", "Frank", "Georgia"]
+trial_patients = ["Denise", "Eddie", "Frank", "Georgia", "Kenny"]
 
 # remove warfarin and add edoxaban
 for patient in trial_patients:
     prescription = patients[patient]
-    prescription.remove(warfarin)
-    prescription.add(edoxaban)
+    try:
+        prescription.remove(warfarin)
+        prescription.add(edoxaban)
+    except KeyError:
+        print(f"Patient {patient} is not taking warfarin."
+              f"Please remove {patient} from the trial")
     print(patient, prescription)
