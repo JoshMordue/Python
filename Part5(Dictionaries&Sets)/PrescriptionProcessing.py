28a96c106eb4d3952bfe8508e3d4601e051ceb4d
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-15 17:06:12 +0100

    Update PrescriptionProcessing.py

--- Part5(Dictionaries&Sets)/PrescriptionProcessing.py
+++ Part5(Dictionaries&Sets)/PrescriptionProcessing.py
@@ -1,5 +1,9 @@
+from PrescriptionData import patients
+
 trial_patients = {"Denise", "Eddie", "Frank", "Georgia", "Kenny"}
 
 while trial_patients:
     patient = trial_patients.pop()
-    print(patient)
+    print(patient, end=" : ")
+    prescription = patients[patient]
+    print(prescription)
