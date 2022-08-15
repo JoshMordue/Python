cafa41e1d2432bca7aca89b0f59672b1c071b4da
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-15 17:36:07 +0100

    Update SetUnion.py

--- Part5(Dictionaries&Sets)/SetUnion.py
+++ Part5(Dictionaries&Sets)/SetUnion.py
@@ -16,6 +16,8 @@ meds_to_watch = set()
 
 for interaction in adverse_interactions:
     # meds_to_watch = meds_to_watch.union(interaction)
-    meds_to_watch = meds_to_watch | interaction
+    # meds_to_watch = meds_to_watch | interaction
+    # meds_to_watch.update(interaction)
+    meds_to_watch |= interaction
 
 print(sorted(meds_to_watch))
