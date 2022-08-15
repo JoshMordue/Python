7f42d9ded2cbeb6aa35a0304002dd88d16de12b8
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-15 23:58:50 +0100

    Create SuperSub.py

---
+++ Part5(Dictionaries&Sets)/SuperSub.py
@@ -0,0 +1,17 @@
+animals = {'Turtle', 'Horse', 'Robin', 'Python', 'Swallow', 'Hedgehog', 'Wren', 'Aardvark', 'Cat'}
+birds = {'Robin', 'Swallow', 'Wren'}
+
+print(f'Birds is a subset of animals: {birds.issubset(animals)}')
+print(f'Animals is a superset of birds: {animals.issuperset(birds)}')
+print()
+print(f'Birds is a superset of animals: {birds.issuperset(animals)}')
+
+print(birds <= animals)
+print(birds < animals)
+
+print("*" * 70)
+
+garden_birds = {'Swallow', 'Wren', 'Robin'}
+print(garden_birds == birds)
+print(garden_birds <= birds)
+print(garden_birds < birds)
