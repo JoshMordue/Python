93c980735a3cd8bf7d2c7920815f21a948a40784
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-16 17:16:00 +0100

    Update ReadPoem.py

--- Part6(Files)/ReadPoem.py
+++ Part6(Files)/ReadPoem.py
@@ -6,7 +6,16 @@
 #
 # jabber.close()
 
-with open('Jabberwocky.txt', 'r') as jabber:
+# with open('Jabberwocky.txt', 'r') as jabber:
+#
+#     # for line in jabber:
+#     #     print(line.rstrip())
+#
+#     lines = jabber.readlines()
+#
+# print(lines[-1:])
+# print()
+#
+# for line in reversed(lines):
+#     print(line, end=' ') do some processing in reverse order
 
-    for line in jabber:
-        print(line.rstrip())
