b7a0d12b7c60c2a6d75c201a71a7d11ea2b572b0
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-16 18:59:55 +0100

    Create Strip.py

---
+++ Part6(Files)/Strip.py
@@ -0,0 +1,10 @@
+filename = 'Jabberwocky.txt'
+with open(filename) as poem:
+    first = poem.readline().rstrip()
+
+
+print(first)
+chars = "'Twasebv"
+
+no_apostrophe = first.strip(chars)
+print(no_apostrophe)
