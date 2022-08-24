f434707d6b2cfa44775153c86e886787eb06210d
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-24 21:28:58 +0100

    Update ReadPoem.py

--- Part6(Files)/ReadPoem.py
+++ Part6(Files)/ReadPoem.py
@@ -1,9 +1,9 @@
-# jabber = open('Jabberwocky.txt', 'r')
-#
-# for line in jabber:
-#     print(line.strip())
-#
-#
+jabber = open('Jabberwocky.txt', 'r')
+
+for line in jabber:
+    print(line.strip())
+
+
 # jabber.close()
 
 # with open('Jabberwocky.txt', 'r') as jabber:
