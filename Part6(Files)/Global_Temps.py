ce6b15216c434ce5c5233536d328716881def682
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-27 22:55:21 +0100

    Update Global_Temps.py

--- Part6(Files)/Global_Temps.py
+++ Part6(Files)/Global_Temps.py
@@ -5,4 +5,11 @@ json_data_source = 'temperature_anomaly.json'
 with open(json_data_source, encoding='utf-8') as data:
     anomalies = json.load(data)
 
-print(anomalies)
+print(anomalies['description'])
+
+for year, value in anomalies['data'].items():
+    year, value = int(year), float(value)
+    print(f'{year} .. {value:6.2f}')
+    
+print("*" * 80)
+print(anomalies['citation'])
