88b9daf58cb44d193c3b51d895af6af565e700cb
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-08-28 23:22:30 +0100

    Update Global_Temps.py

--- Part6(Files)/Global_Temps.py
+++ Part6(Files)/Global_Temps.py
@@ -10,6 +10,6 @@ print(anomalies['description'])
 for year, value in anomalies['data'].items():
     year, value = int(year), float(value)
     print(f'{year} .. {value:6.2f}')
-    
+
 print("*" * 80)
 print(anomalies['citation'])
