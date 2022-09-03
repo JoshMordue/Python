6b3cc3a1bb9fb9d8b925764695b62eab5c6084c4
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-09-03 20:11:53 +0100

    Update DictReaderChallenge.py

--- Part6(Files)/DictReaderChallenge.py
+++ Part6(Files)/DictReaderChallenge.py
@@ -9,13 +9,14 @@ with open(input_filename, encoding='utf-8', newline='') as country_file:
         countries[row['Country'].casefold()] = row
         countries[row['CC'].casefold()] = row
 
-# print(countries)
+print(countries)
 
 while True:
     chosen_country = input('Please enter the name of a country: ')
     country_key = chosen_country.casefold()
     if country_key in countries:
         country_data = countries[country_key]
-        print(f"The capital of {chosen_country} is {country_data['Capital']}")
+        print(f"The capital of {chosen_country} is {country_data['Capital']} with capital code: {country_data['CC']}")
+
     elif chosen_country == 'quit':
         break
