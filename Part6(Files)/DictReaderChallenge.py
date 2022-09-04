48a3ab4a380c46f7148d9cbf2e6d33475f45479a
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-09-04 21:33:37 +0100

    Update DictReaderChallenge.py

--- Part6(Files)/DictReaderChallenge.py
+++ Part6(Files)/DictReaderChallenge.py
@@ -8,7 +8,10 @@ dialect.delimiter = '|'
 
 countries = {}
 with open(input_filename, encoding='utf-8', newline='') as country_file:
-    dict_reader = csv.DictReader(country_file, delimiter='|')
+    dict_reader = csv.DictReader(country_file, delimiter='|', dialect=dialect)
+    # get the column headings from the first line of the file
+    headings = country_file.readline().strip('\n').split(dialect.delimiter)
+    
     for row in dict_reader:
         countries[row['Country'].casefold()] = row
         countries[row['CC'].casefold()] = row
