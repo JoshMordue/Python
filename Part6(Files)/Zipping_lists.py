f9977381990f8dae61cae8b37eb821ea81aa6ca3
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-09-07 23:56:10 +0100

    Update Zipping_lists.py

--- Part6(Files)/Zipping_lists.py
+++ Part6(Files)/Zipping_lists.py
@@ -8,7 +8,9 @@ albums = [("Welcome to my Nightmare", "Alice Cooper", 1975),
 keys = ['album', 'artist', 'year']
 
 for row in albums:
-    zip_object = keys, row
-    print(zip_object)
-    for thing in zip(keys, row):
-        print("\t", thing)
+    zip_object = zip(keys, row)
+    # print(zip_object)
+    # for thing in zip(keys, row):
+    #     print("\t", thing)
+    albums_dict = dict(zip_object)
+    print(albums_dict)
