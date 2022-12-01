ee06c97723a3058042a3f1d4be2c4a2df246b49d
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-12-01 15:24:54 +0000

    work on challenge

--- AdventofCode/Day1.py
+++ AdventofCode/Day1.py
@@ -1,27 +1,25 @@
-# Elfs = {'First Elf': sum([1000, 2000, 3000]),
-#         'Second Elf': sum([4000]),
-#         'Third Elf': sum([5000, 6000]),
-#         'Fourth Elf': sum([7000, 8000, 9000]),
-#         'Fifth Elf': sum([10000]),
-#         }
+total = 0
+record = 0
+position = 0
+record_amount = 0
 
 
 with open('input.txt', 'r') as elf_list:
-    elfs = {}
-    position = 0
+
     for line in elf_list:
-        if int(line) > 0:
-            ('position', val) = line.split()
-            elf_list[int(position)] = val
+        if line != '\n':
+            total += int(line)
+
         else:
+            if total > record:
+                record = position
+                record_amount = total
+            total = 0
             position += 1
 
-    elf_list.close()
+    print(total)
+    print(record)
+    print(position)
 
 
-
-
-
-
-print("The Elf with the most calories is the " + (max(Elfs.keys(), key=lambda i: i[1])))
-
+elf_list.close()
