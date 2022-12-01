2dc0840e1a9676f1f1017b078f59257aa272c0c2
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-12-01 15:04:26 +0000

    work on project

--- AdventofCode/Day1.py
+++ AdventofCode/Day1.py
@@ -1,9 +1,27 @@
-Elfs = {'First Elf': sum([1000, 2000, 3000]),
-        'Second Elf': sum([4000]),
-        'Third Elf': sum([5000, 6000]),
-        'Fourth Elf': sum([7000, 8000, 9000]),
-        'Fifth Elf': sum([10000]),
-        }
+# Elfs = {'First Elf': sum([1000, 2000, 3000]),
+#         'Second Elf': sum([4000]),
+#         'Third Elf': sum([5000, 6000]),
+#         'Fourth Elf': sum([7000, 8000, 9000]),
+#         'Fifth Elf': sum([10000]),
+#         }
+
+
+with open('input.txt', 'r') as elf_list:
+    elfs = {}
+    position = 0
+    for line in elf_list:
+        if int(line) > 0:
+            ('position', val) = line.split()
+            elf_list[int(position)] = val
+        else:
+            position += 1
+
+    elf_list.close()
+
+
+
+
+
 
 print("The Elf with the most calories is the " + (max(Elfs.keys(), key=lambda i: i[1])))
 
