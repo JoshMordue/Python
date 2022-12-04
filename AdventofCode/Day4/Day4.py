6c74c6810200008f9ffcdf17a9b485088420450a
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-12-04 20:33:45 +0000

    Update Day4.py

--- AdventofCode/Day4/Day4.py
+++ AdventofCode/Day4/Day4.py
@@ -5,10 +5,12 @@ filename = 'input.txt'
 with open(filename) as f:
     lines = [list(map(int, re.split('-|,', line.rstrip()))) for line in f]
 
+
 overlap_count_1 = 0
 overlap_count_2 = 0
 
 for this_pair in lines:
+    print(this_pair)
     elf_1 = set([n for n in range(this_pair[0], this_pair[1] + 1)])
     elf_2 = set([n for n in range(this_pair[2], this_pair[3] + 1)])
 
@@ -20,3 +22,4 @@ for this_pair in lines:
 
 print("Part 1:", overlap_count_1)
 print("Part 2:", overlap_count_2)
+
