df5f9f37659459f0868e4a46a2793e7a513aac2e
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-12-05 15:33:27 +0000

    Update Day5.py

--- AdventofCode/Day5/Day5.py
+++ AdventofCode/Day5/Day5.py
@@ -1,49 +1,21 @@
-with open('input.txt', 'rt') as instructions:
-    crates, ins = instructions.read().split("\n\n")
+with open("input.txt") as f:
+    lines = f.readlines()
 
-stack_width = 4
-p = re.compile(r"[A-Z]")
-# reverse it, so we've got the stack numbers at the top
-stack_data = stack_data[::-1]
-num_stacks = len(stack_data[0].split())
+idx = 0
+for i, l in enumerate(lines):
+    if len(l) == 1:
+        idx = i
+        break
 
+crates = list(zip(*[list(l[1::4]) for l in lines[i-2::-1]]))
+crates = [[k for k in c if k != ' '] for c in crates]
 
+moves = [l.split(" from ") for l in lines[idx+1:]]
+for a, b in moves:
+    idx_s, idx_t = [int(k)-1 for k in b.split(" to ")]
+    s, t = crates[idx_s], crates[idx_t]
+    r = [s.pop() for _ in range(int(a[5:]))]
+    #t.extend(r)      # Part 1
+    t.extend(r[::-1]) # Part 2
 
-movements = crates(ins.splitlines())
-
-print(stacks)
-print(movements)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# stack1 = ["D","L","J","R","V","G","F",]
-        # stack2 = ["",]
-        # stack3 = ["R","W","Z","H","Q"]
-        # stack4 = ["R","Z","J","V","D","W"]
-        # stack5 = ["B","M","H","S"]
-        # stack6 = ["B","P","V","H","J","N","G","L"]
-        # stack7 = ["S","L","D","H","F","Z","Q","J"]
-        # stack8 = ["B","Q","G","J","F","S","W"]
-        # stack9 = ["J","D","C","S","M","W","Z"]
-
+print("".join(c[-1] for c in crates))
