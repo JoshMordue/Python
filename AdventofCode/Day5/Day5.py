e55a3ff3193ff526d64b5e7327065311117398f4
JoshMordue <joshua.mordue@hotmail.co.uk>
2022-12-05 11:22:56 +0000

    day5 work

---
+++ AdventofCode/Day5/Day5.py
@@ -0,0 +1,31 @@
+index = 1
+with open('input.txt', 'r') as instructions:
+    for line, crate in enumerate(instructions):
+        if line < 10:
+            crates = instructions.readline()[0::3]
+            print(crates)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # stack1 = ["D","L","J","R","V","G","F",]
+        # stack2 = ["",]
+        # stack3 = ["R","W","Z","H","Q"]
+        # stack4 = ["R","Z","J","V","D","W"]
+        # stack5 = ["B","M","H","S"]
+        # stack6 = ["B","P","V","H","J","N","G","L"]
+        # stack7 = ["S","L","D","H","F","Z","Q","J"]
+        # stack8 = ["B","Q","G","J","F","S","W"]
+        # stack9 = ["J","D","C","S","M","W","Z"]
+
